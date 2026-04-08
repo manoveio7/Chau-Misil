97487411d7702e7abe5669ce8375cb4d5ae5f5b4
manoveio7 <85594759+manoveio7@users.noreply.github.com>
2026-04-07 23:43:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,73 @@
-const CACHE_NAME = 'chau-misil-v1';
-// Lista de todos los archivos de tu juego que quieres guardar
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icono.png',
-  './js/juego.js',       // Revisa que la ruta sea correcta
-  './img/fondo.png',     // Agrega aquí todas tus imágenes
-  './audio/musica.mp3'   // Agrega aquí tus sonidos si tienes
+  './sw.js',
+  
+  // SCRIPTS (Carpeta js)
+  './js/comandoVoz.js',
+  './js/comdevoz.js',
+  './js/console.js',
+  './js/console2.js',
+  './js/entrada.js',
+  './js/inicio.js',
+  './js/libreriaVoz.js',
+  './js/main.js',
+  './js/phaser.js',
+  './js/phaser.min.js',
+
+  // SONIDOS (Carpeta audio)
+  './audio/AudioTiroAvion.mp3',
+  './audio/apareceMisil.mp3',
+  './audio/audioArma.mp3',
+  './audio/estrella.mp3',
+  './audio/explosion.mp3',
+  './audio/motor.mp3',
+  './audio/motor_2.mp3',
+  './audio/peligro.mp3',
+  './audio/peligro2.mp3',
+  './audio/toca.mp3',
+
+  // IMÁGENES (Carpeta img)
+  './img/PanelAzul.png',
+  './img/aguja.png',
+  './img/alerta.png',
+  './img/alertaArma.png',
+  './img/arma.png',
+  './img/avion.png',
+  './img/bala.png',
+  './img/blanco.png',
+  './img/brillo.png',
+  './img/brillo2.png',
+  './img/btn.png',
+  './img/btnBala.png',
+  './img/btnBorrar.png',
+  './img/btnOk.png',
+  './img/cielo.png',
+  './img/entrada.png',
+  './img/estrella.png',
+  './img/explosion.png',
+  './img/flares.json',
+  './img/flares.png',
+  './img/fuel.png',
+  './img/fuel2.png',
+  './img/fuelAlerta.png',
+  './img/fuelBase.png',
+  './img/gameOver.png',
+  './img/llave.png',
+  './img/menu.png',
+  './img/misil.png',
+  './img/panel.png',
+  './img/panel1.png',
+  './img/panel2.png',
+  './img/panelEstrellas.png',
+  './img/panelMisiles.png',
+  './img/panelReloj.png',
+  './img/play.png',
+  './img/play2.png',
+  './img/reinstalar.png',
+  './img/reloj.png',
+  './img/rojo.png',
+  './img/starVerde.png'
 ];
-
-// 1. Instalar y guardar archivos en caché
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// 2. Hacer que el juego funcione sin internet (Modo Offline)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Si el archivo está en la memoria (caché), lo usa. Si no, lo busca en internet.
-      return response || fetch(event.request);
-    })
-  );
-});
