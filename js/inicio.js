dc4b45087935f87d8d8bf2c3adb9b4c8a6847c95
manoveio7 <85594759+manoveio7@users.noreply.github.com>
2026-04-15 14:41:50 -0300

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -28,13 +28,15 @@ var config = {
 	type: Phaser.AUTO,
 	width: window.innerWidth,
 	height: window.innerHeight,
-	
+	scale: {
+        // Ajusta el juego al tamaño de la ventana conservando la proporción
+        mode: Phaser.Scale.FIT, 
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
 	fps: {
 	target: 60,
 	forceSetTimeOut: true
 	},
-	
-	
 	physics: {
 		default: 'arcade',
 		arcade: {
